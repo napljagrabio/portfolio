f2af0011dca81225809ad7903c86553263c460fa
napljagrabio <145481508+napljagrabio@users.noreply.github.com>
2024-04-11 19:22:11 +0800

    test

---
+++ forms/contact.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Requires the "PHP Email Form" library
+ * The "PHP Email Form" library is available only in the pro version of the template
+ * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
+
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'louiejie.agrabio@gmail.com';
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Construct email headers
+    $headers = "From: $name <$email>" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // Send email
+    if (mail($receiving_email_address, $subject, $message, $headers)) {
+        echo "Email sent successfully.";
+    } else {
+        echo "Failed to send email.";
+    }
+}
